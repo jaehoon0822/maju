b69e04bb2fe983e4eeaaadfbe9333c944766889e
jhoon <jaehoon0822@gmail.com>
2023-08-12 18:34:52 +0900

    userService 주석 내용 추가

--- backend/src/services/User.ts
+++ backend/src/services/User.ts
@@ -1,10 +1,24 @@
 import { AppDataSource } from "@/index";
 import { User } from "@/entities/User";
-import { UnauthorizedError } from "@/errors/unauthorized-error";
 
+// TypeORM 의 repository 생성
 export const UserRepo = AppDataSource.getRepository(User);
 
+// User 를 위한 서비스 로직을 가진 Class 생성
 class UserService {
+  /***
+   *
+   * @remarks
+   * user 를 찾는 서비스
+   *
+   * @param email
+   * - 첫번째 input 은 email
+   *
+   * @returns Promise<User | null | undefined>
+   *  - User 를 반환 혹은 null or undefined 반환
+   *
+   */
+
   public async find(email: string) {
     try {
       const result = await UserRepo.createQueryBuilder("user")
@@ -17,6 +31,20 @@ class UserService {
       if (error instanceof Error) throw new Error(error.message);
     }
   }
+  /***
+   *
+   * @remarks
+   * user 를 생성하는 서비스
+   *
+   * @param email
+   * - 첫번째 input 은 string
+   * @param password
+   * - 두번째 input 은 string
+   *
+   * @returns Promise<InsertResult | null | undefined>
+   *  - Promise 인 InsertResult 및 null, undeffined 반환
+   *
+   */
   public async create(email: string, password: string) {
     try {
       const result = await UserRepo.createQueryBuilder("user")
