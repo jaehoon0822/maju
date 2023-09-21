57e3948ac8c14a51f0bd5f8f48bb4dd5b62f5e8a
jhoon <jaehoon0822@gmail.com>
2023-09-21 22:44:53 +0900

    backend 완료

--- frontend/src/common/validation/login.yup.ts
+++ frontend/src/common/validation/login.yup.ts
@@ -1,20 +1,4 @@
 import * as yup from "yup";
-<<<<<<< HEAD
-
-export const loginSchema = yup.object({
-  email: yup
-    .string()
-    .required("내용을 입력해주세요.")
-    .min(2, "최소 2자 이상 입력해주세요.")
-    .max(20, "최대 20자 이하로 입력해주세요.")
-    .email("이메일 형식에 맞춰 작성해주세요."),
-  password: yup
-    .string()
-    .required("내용을 입력해주세요.")
-    .min(6, "최소 6자 이상 입력해주세요.")
-    .max(16, "최대 16자 이상 입력해주세요."),
-});
-=======
 import { emailSchema } from "./email.yup";
 import { passwordSchema } from "./password.yup";
 
@@ -25,4 +9,3 @@ export const loginSchema = yup
   .omit(["passwordConfirm"]);
 
 export type loginSchemaType = yup.InferType<typeof loginSchema>;
->>>>>>> feature
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,59 +1,18 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Image from "next/image";
 import { Noto_Sans } from "next/font/google";
 import { loginSchema } from "@/common/validation/login.yup";
 import { Input } from "@/components/Atomic/Atom/Input";
 import { Form } from "@/components/Atomic/Atom/Form/Index";
-import { Button } from "@/components/Atomic/Atom/Button";
 import FormButton from "@/components/Atomic/Atom/FormButton";
-import TextButton from "@/components/Atomic/Atom/TextButton";
-
-export default function Home() {
-  return (
-    <main>
-      <h1>hi</h1>
-      <Form
-        onSubmit={(data) => {
-          console.log(data);
-        }}
-        schema={loginSchema}
-      >
-        <Input label="email" name="email" id="email" placeholder="Email" />
-        <Input
-          label="password"
-          name="password"
-          id="password"
-          placeholder="Password"
-          type="password"
-        />
-        <FormButton variant="kakao" label="로그인" />
-        <TextButton lable="testtest" href={"/login"} />
-      </Form>
-=======
 import TextButton from "@/components/Atomic/Atoms/TextButton";
-=======
->>>>>>> feature
 import LoginTemplate from "@/components/Atomic/Templates/LoginTemplate";
 import Head from "next/head";
 
 export default function Index() {
   return (
-<<<<<<< HEAD
     <main>
       <LoginTemplate />
       <TextButton href="/home" label="test" />
->>>>>>> feature
     </main>
-=======
-    <>
-      <Head>
-        <title>MAJU 로그인</title>
-      </Head>
-      <main>
-        <LoginTemplate />
-      </main>
-    </>
->>>>>>> feature
   );
 }
